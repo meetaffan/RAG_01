baf3d8a985f81a0f16edeb0333d06838ecab824b
affan <meetaffan@users.noreply.github.com>
2026-01-06 22:27:41 +0530

    Initial gpt 5 wrapper

--- main.py
+++ main.py
@@ -1,9 +1,57 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+import os
+from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import SystemMessage, UserMessage
+from azure.core.credentials import AzureKeyCredential
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 file_path = "./nodejs.pdf"
-loader = PyPDFLoader(file_path)
 
+# Loading
+loader = PyPDFLoader(file_path)
 docs = loader.load()
-print(docs[5])
+# print(docs[5])
+
+# Chunking
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 200
+)
+
+split_docs = text_splitter.split_documents(documents=docs)
+
+# Vector Embeddings
+# embedding_model = OpenAIEmbeddings(
+#     model="text-embedding-3-large",
+#     # With the `text-embedding-3` class
+#     # of models, you can specify the size
+#     # of the embeddings you want returned.
+#     # dimensions=1024
+# )
+
+endpoint = "https://models.github.ai/inference"
+# model = "openai/gpt-5-mini"
+model = "gpt-5-mini"
+token = os.environ["GITHUB_TOKEN"]
+
+client = ChatCompletionsClient(
+    endpoint=endpoint,
+    credential=AzureKeyCredential(token),
+)
+
+response = client.complete(
+    messages=[
+        SystemMessage("You are a helpful assistant."),
+        UserMessage("What is the capital of France?"),
+    ],
+    model=model
+)
+
+print(response.choices[0].message.content)
+
 
